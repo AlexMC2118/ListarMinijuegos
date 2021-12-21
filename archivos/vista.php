4fac79ddb666afea6e7896c605d653c4faf4fad7
Alejandro <amorenocamacho.guadalupe@alumnado.fundacionloyola.net>
2021-12-21 21:54:33 +0100

    Ranking directo

--- archivos/vista.php
+++ archivos/vista.php
@@ -13,6 +13,7 @@
             <div>
               <a href="'.$resultado['ruta'].'?idMinijuego='.$resultado['idMinijuego'].'"><img src="'.$resultado['portada'].'.png" /></a>
               <h2>'.$resultado['nombre'].'</h2>
+              <a href="../RankingPorMinijuego?idMinijuego='.$resultado['idMinijuego'].'" class="ranking">Ranking</a>
               <a href="'.$resultado['ruta'].'?idMinijuego='.$resultado['idMinijuego'].'"><img class="oculta" src="img/play.png" /></a>
             </div>
           ';
